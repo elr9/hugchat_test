12f6b746a8e7b3a53e241081efd02a7a8f36a1b6
elr9 <90631728+elr9@users.noreply.github.com>
2023-05-24 21:15:13 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,5 @@ from streamlit_chat import message
 from streamlit_extras.colored_header import colored_header
 from streamlit_extras.add_vertical_space import add_vertical_space
 from hugchat import hugchat
+
+st.set_page_config(page_title="Chat_test")
